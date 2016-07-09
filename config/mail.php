aff93f7e22b7674f6ec8c0c697fdfaf0bfbf5c79
billythekid <billy@the-kid.org>
2016-07-09 19:46:31 +0100

    mail name and email addy

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'billy@aberdeenwebsolutions.co.uk', 'name' => 'Billy Fagan'],
 
     /*
     |--------------------------------------------------------------------------
