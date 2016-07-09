ada5f7993dd7940057cbd1c065ca8164c9863cf6
billythekid <billy@the-kid.org>
2016-07-09 20:13:46 +0100

    example logins

--- resources/views/partials/logins.blade.php
+++ resources/views/partials/logins.blade.php
@@ -1,23 +1,26 @@
-<div class="row">
-    <div class="col-xs-12">
-        <p>You can use the following login details to test this, or just make an account.</p>
+@if(Auth::guest())
+    <div class="row">
+        <div class="col-xs-12">
+            <p>You can use the following login details to test this, or just make an account.</p>
+        </div>
+        <div class="col-sm-6">
+            <ul>
+                <li>test@example.com</li>
+                <li>secret</li>
+            </ul>
+        </div>
+        <div class="col-sm-6">
+            <ul>
+                <li>example@example.com</li>
+                <li>secret</li>
+            </ul>
+        </div>
+        <div class="col-xs-12">
+            <p>
+                Ideally, you can log in using one set of details then open an incognito window or a different browser and log in
+                using
+                the other details to watch the live-updates on items.
+            </p>
+        </div>
     </div>
-    <div class="col-sm-6">
-        <ul>
-            <li>test@example.com</li>
-            <li>secret</li>
-        </ul>
-    </div>
-    <div class="col-sm-6">
-        <ul>
-            <li>example@example.com</li>
-            <li>secret</li>
-        </ul>
-    </div>
-    <div class="col-xs-12">
-        <p>
-            Ideally, you can log in using one set of details then open an incognito window or a different browser and log in using
-            the other details to watch the live-updates on items.
-        </p>
-    </div>
-</div>
+@endif
