47d10e4e3f499e5c431a0d5614e9b227bbbe7334
billythekid <billy@the-kid.org>
2016-07-09 19:22:34 +0100

    alts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,7 +49,7 @@
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- Left Side Of Navbar -->
             <ul class="nav navbar-nav">
-                <li><a href="{{ url('/home') }}">Home</a></li>
+                {{--<li><a href="{{ url('/home') }}">Home</a></li>--}}
             </ul>
 
             <!-- Right Side Of Navbar -->
