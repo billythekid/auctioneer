70fb1d8a84ababcc62e5e6729750b98640a24d8d
billythekid <billy@the-kid.org>
2016-07-10 21:58:25 +0100

    commentz

--- socket.js
+++ socket.js
@@ -12,6 +12,7 @@ redis.on('message', function (channel, message) {
     io.emit(channel + message.data.item_id + ':' + message.event, message.data);
 });
 
+// This bit here can be 'tricked' with rapid refreshes, but eventually all ghost connections time out so i think it's fine
 var usersConnected = 0;
 io.on('connection', function (socket) {
     usersConnected++;
@@ -21,6 +22,5 @@ io.on('connection', function (socket) {
     });
     io.emit('visitorsConnected', usersConnected);
 });
-
 
 server.listen(3000);
